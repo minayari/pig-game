8d421bb50b2272b85158cf19be2fa79eafe4436f
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-09-01 16:23:29 +0330

    dynamic scoring and styles

--- script.js
+++ script.js
@@ -3,29 +3,56 @@
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const dice = document.querySelector('.dice');
-const currentLable = document.querySelector('#current--0');
-const scoreLable = document.querySelector('#score--0');
 
-let currentValue = JSON.parse(localStorage.getItem('currentValue0')) || 0;
-let holdValue = JSON.parse(localStorage.getItem('currentValue0')) || 0;
+let current0 = 0;
+let current1 = 0;
+let total0 = JSON.parse(localStorage.getItem('total0')) || 0;
+let total1 = JSON.parse(localStorage.getItem('total1')) || 0;
+
+let playerNumber = 0;
 
 function setDice() {
   const DICE_NUMBER = Math.floor(Math.random() * 6) + 1;
 
   dice.src = `./dice-${DICE_NUMBER}.png`;
 
-  currentValue += DICE_NUMBER;
-  localStorage.setItem('currentValue0', JSON.stringify(currentValue));
-  currentLable.textContent = currentValue;
+  addCurrent(DICE_NUMBER);
+}
+
+function addCurrent(dice) {
+  if (!playerNumber) {
+    current0 += dice;
+    document.querySelector('#current--0').textContent = current0;
+  } else {
+    current1 += dice;
+    document.querySelector('#current--1').textContent = current1;
+  }
 }
 
 function setHoldValue() {
-  holdValue += currentValue;
-  localStorage.setItem('holdValue0', JSON.stringify(holdValue));
-  scoreLable.textContent = holdValue;
+  if (!playerNumber) {
+    total0 += current0;
+    localStorage.setItem('total0', JSON.stringify(total0));
+    document.querySelector('#score--0').textContent = total0;
+    document.querySelector('#current--0').textContent = '0';
+    current0 = 0;
 
-  currentLable.textContent = 0;
-  currentValue = 0;
+    document.querySelector('.player--0').classList.remove('player--active');
+    document.querySelector('.player--1').classList.add('player--active');
+
+    playerNumber = !playerNumber;
+  } else {
+    total1 += current1;
+    localStorage.setItem('total1', JSON.stringify(total1));
+    document.querySelector('#score--1').textContent = total1;
+    document.querySelector('#current--1').textContent = '0';
+    current1 = 0;
+
+    document.querySelector('.player--1').classList.remove('player--active');
+    document.querySelector('.player--0').classList.add('player--active');
+
+    playerNumber = !playerNumber;
+  }
 }
 
 btnRoll.addEventListener('click', setDice);
