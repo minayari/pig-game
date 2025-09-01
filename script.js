d95d99ca99f6fbdeedd187fb5c40573d186c0ca6
MinaAllahyari <minaallahyari.uni@gmail.com>
2025-09-01 17:09:40 +0330

    conditional remove at dice one

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const dice = document.querySelector('.dice');
 
+const currentElement0 = document.querySelector('#current--0');
+const currentElement1 = document.querySelector('#current--1');
+const totalElement0 = document.querySelector('#score--0');
+const totalElement1 = document.querySelector('#score--1');
+
 let current0 = 0;
 let current1 = 0;
 let total0 = JSON.parse(localStorage.getItem('total0')) || 0;
@@ -20,12 +25,32 @@ function setDice() {
 }
 
 function addCurrent(dice) {
-  if (!playerNumber) {
-    current0 += dice;
-    document.querySelector('#current--0').textContent = current0;
+  if (dice !== 1) {
+    if (!playerNumber) {
+      current0 += dice;
+      currentElement0.textContent = current0;
+    } else {
+      current1 += dice;
+      currentElement1.textContent = current1;
+    }
   } else {
-    current1 += dice;
-    document.querySelector('#current--1').textContent = current1;
+    if (!playerNumber) {
+      current0 = 0;
+      total0 = 0;
+      localStorage.setItem('total0', total0);
+      currentElement0.textContent = '0';
+      totalElement0.textContent = '0';
+      changePlayerActive();
+      playerNumber = !playerNumber;
+    } else {
+      current1 = 0;
+      total1 = 0;
+      localStorage.setItem('total1', total1);
+      currentElement1.textContent = '0';
+      totalElement1.textContent = '0';
+      changePlayerActive();
+      playerNumber = !playerNumber;
+    }
   }
 }
 
@@ -33,25 +58,31 @@ function setHoldValue() {
   if (!playerNumber) {
     total0 += current0;
     localStorage.setItem('total0', JSON.stringify(total0));
-    document.querySelector('#score--0').textContent = total0;
-    document.querySelector('#current--0').textContent = '0';
+    totalElement0.textContent = total0;
+    currentElement0.textContent = '0';
     current0 = 0;
-
-    document.querySelector('.player--0').classList.remove('player--active');
-    document.querySelector('.player--1').classList.add('player--active');
+    changePlayerActive();
 
     playerNumber = !playerNumber;
   } else {
     total1 += current1;
     localStorage.setItem('total1', JSON.stringify(total1));
-    document.querySelector('#score--1').textContent = total1;
-    document.querySelector('#current--1').textContent = '0';
+    totalElement1.textContent = total1;
+    currentElement1.textContent = '0';
     current1 = 0;
-
-    document.querySelector('.player--1').classList.remove('player--active');
-    document.querySelector('.player--0').classList.add('player--active');
+    changePlayerActive();
 
     playerNumber = !playerNumber;
+  }
+}
+
+function changePlayerActive() {
+  if (!playerNumber) {
+    document.querySelector('.player--0').classList.remove('player--active');
+    document.querySelector('.player--1').classList.add('player--active');
+  } else {
+    document.querySelector('.player--1').classList.remove('player--active');
+    document.querySelector('.player--0').classList.add('player--active');
   }
 }
 
